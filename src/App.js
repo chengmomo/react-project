d21984b6661361ced4040c57f3cf509405b60403
18252030586 <z923891099@icloud.com>
2019-08-22 11:36:40 +0800

    自定义主题_config-overrides.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
